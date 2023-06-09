503626c010e63b468b135db3e8c7b92ad294dab0
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-06-09 15:18:01 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+let newMessage=[];
+
+
+function writeCards(names,event){
+    for(let i=0; i<names.length; i++){
+     let message =`Thank you, ${names[i]}, for the wonderful ${event} gift!`
+     newMessage.push(message)
+    }
+
+    return newMessage;
+}
+
+
+
+let countDown= (number)=>{
+    for( let i= number; i>= 0; i--){
+        console.log(i)
+    }
+ 
+}
+
+countDown(10)
